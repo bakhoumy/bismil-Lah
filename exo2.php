5d2db457513de850cd4416f00401ed252e61e388
bakhoumy <161771896+bakhoumy@users.noreply.github.com>
2024-02-29 22:06:41 +0000

    Add files via upload

---
+++ exo2.php
@@ -0,0 +1,22 @@
+<?php
+$num = 5;
+$som = 0;
+for ($i =1; $i<=$num; $i++)
+{
+    $som = $som +$i;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo $som ;
+?>
+    
+</body>
+</html>
